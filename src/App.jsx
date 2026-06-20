10821b298718a11a6e55642f757785d8e916efaf
Mohd Zaid <zaidbasic5540@gmail.com>
2026-06-20 10:22:14 +0530

    Initial React project setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>University Evaluation Management System</h1>
+      <p>Project under development</p>
+    </div>
+  )
+}
+
+export default App
